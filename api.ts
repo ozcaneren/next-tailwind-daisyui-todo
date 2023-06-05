cbae98e1878ec5704c51496ee863f31ac1ef78a8
eren ozcan <erenozcandev@gmail.com>
2023-06-05 08:08:11 +0300

    add delete

--- api.ts
+++ api.ts
@@ -20,3 +20,21 @@ export const addTodo = async (todo: ITask): Promise<ITask> => {
   return newTodo;
 }
 
+export const editTodo = async (todo: ITask): Promise<ITask> => {
+  const res = await fetch (`${baseUrl}/tasks/${todo.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  })
+  const updatedTodo = await res.json();
+  return updatedTodo;
+}
+
+export const deleteTodo = async (id: string): Promise<void> => {
+  await fetch (`${baseUrl}/tasks/${id}`, {
+    method: 'DELETE',
+  })
+}
+
--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import { ITask } from "@/types/tasks";
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import Modal from "./Modal";
+import { addTodo, deleteTodo, editTodo } from "@/api";
+import { useRouter } from "next/navigation";
+
 
 interface TaskProps {
   task: ITask
@@ -14,10 +17,24 @@ const Task: React.FC<TaskProps> = ({ task }) => {
 
   const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
   const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
-
+  const router = useRouter();
   const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
 
-  const handleSubmitEditTodo = () => {}
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+    setOpenModalEdit(false);
+    router.refresh();
+  };
+
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDeleted(false);
+    router.refresh();
+  }
 
   return (
     <tr key={task.id}>
@@ -46,11 +63,23 @@ const Task: React.FC<TaskProps> = ({ task }) => {
             </div>
           </form>
         </Modal>
-        <FiTrash2 
+        <FiTrash2
+          onClick={() => setOpenModalDeleted(true)} 
           cursor='pointer' 
           className="text-red-500" 
           size={23}
         />
+        <Modal modalOpen={openModalDeleted} setModalOpen={setOpenModalDeleted}>
+          <h3 className="text-lg">Are you sure, you want to delete this task?</h3>
+          <div className="modal-action">
+            <button 
+              onClick={() => handleDeleteTask(task.id)}
+              className="btn"
+            >
+              Yes
+            </button>
+          </div>
+        </Modal>
       </td>
     </tr>
   )

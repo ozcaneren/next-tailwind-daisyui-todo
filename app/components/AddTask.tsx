a55453b369c10e23634203905d3abbf3f5167562
eren ozcan <erenozcandev@gmail.com>
2023-06-05 07:49:30 +0300

    add editable

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -3,17 +3,27 @@
 import { AiOutlinePlus } from 'react-icons/ai'
 import Modal from './Modal'
 import { FormEventHandler, useState } from 'react'
+import { addTodo } from '@/api'
+import { useRouter } from 'next/navigation'
+import { v4 as uuidv4 } from 'uuid'
 
 const AddTask = () => {
+
+  const router = useRouter();
 
   const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   const [newTaskValue, setNewTaskValue] = useState<string>('');
 
-  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (e) => {
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    console.log(newTaskValue);
+    await addTodo({
+      id: uuidv4(),
+      text: newTaskValue
+    });
     setNewTaskValue('');
+    setModalOpen(false);
+    router.refresh();
   }
 
   return (
--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import { ITask } from "@/types/tasks";
-import React from "react";
+import React, { useState } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+import Modal from "./Modal";
 
 interface TaskProps {
   task: ITask
@@ -7,10 +11,47 @@ interface TaskProps {
 
 
 const Task: React.FC<TaskProps> = ({ task }) => {
+
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
+
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEditTodo = () => {}
+
   return (
     <tr key={task.id}>
-      <td>{task.text}</td>
-      <td>Blue</td>
+      <td className="w-full">{task.text}</td>
+      <td className="flex gap-5">
+        <FiEdit 
+          onClick={() => setOpenModalEdit(true)} 
+          cursor='pointer' 
+          className="text-blue-500" 
+          size={23}
+        />
+        <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className='font-bold text-lg'>Edit task</h3>
+            <div className='modal-action'>
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type='text'
+                placeholder='Type here'
+                className='input input-bordered w-full'
+              />
+              <button type='submit' className='btn'>
+                Submit
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <FiTrash2 
+          cursor='pointer' 
+          className="text-red-500" 
+          size={23}
+        />
+      </td>
     </tr>
   )
 }
